d58bccd59dc11af4a112ed93e77e3ddf409db9ce
clay-to-n <cbrand@usc.edu>
2014-04-12 05:38:17 -0700

    Committing sample Spritz project folder

---
+++ LitSpritz/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Drew Ingebretsen on 4/10/14.
+//  Copyright (c) 2014 Spritz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface ViewController : UIViewController
+
+
+@end
